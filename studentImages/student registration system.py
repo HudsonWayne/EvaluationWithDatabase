16b707817b74e8423113fec56a78060b194495bc
HudsonWayne <waynewaynebenhura@gmail.com>
2025-04-16 12:19:08 +0200

    importing modules

--- studentImages/student registration system.py
+++ studentImages/student registration system.py
@@ -2,3 +2,10 @@ from tkinter import *
 from datetime import date
 from tkinter import filedialog
 from tkinter import messagebox
+from PIL import Image, ImageTk
+import os
+
+from tkinter.ttk import Combobox
+import openpyxl,xlrd
+from openpyxl import workbook
+import pathlib
