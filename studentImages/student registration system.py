c9a850bc106cb05887cac97d218985a8c30cb03a
HudsonWayne <waynewaynebenhura@gmail.com>
2025-04-16 12:32:26 +0200

    initial commit

--- studentImages/student registration system.py
+++ studentImages/student registration system.py
@@ -9,3 +9,15 @@ from tkinter.ttk import Combobox
 import openpyxl,xlrd
 from openpyxl import workbook
 import pathlib
+
+
+background = "#06283D"
+framebg="#EDEDED"
+framebg="#06283D"
+
+root = Tk()
+root.title("Student Registration System")
+root.geometry("1250x700+210+100")
+root.config(bg=background)
+
+root.mainloop()
